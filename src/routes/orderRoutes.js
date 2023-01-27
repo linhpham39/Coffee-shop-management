59b0b29da9a6a6b192cc1e60f1f6d3c3c75c9e7a
linhpham39 <linhshark02@gmail.com>
2023-01-28 00:34:01 +0700

    tao file orders

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,15 @@
+const {Router} = require('express');
+const orders = require('../controllers/orderController');
+var router = Router();
+
+router.get('/', orders.showOrders);
+
+// các chức năng của order:
+/* 
+- add 1 order
+- delete 1 order
+- show ra các orderlines của order đó dựa trên order_id
+- show ra các order trong 1 ngày
+- tính tổng doanh thu từ date1 đến date 2
+- tính tổng lợi nhuận từ date 1 đến date 2
+*/
