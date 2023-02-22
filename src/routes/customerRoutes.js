0fcb7bb70f3458f57c0aa6db3dd501c6402aac0a
MinhNgo <quangminhngo3@gmail.com>
2023-02-22 20:38:38 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/linhpham39/Coffee-shop-management""
    This reverts commit d828ea1b5194adc89ffc6ae5ff88ffbf57eabc2d.

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -6,10 +6,9 @@ const verifyRoles = require('../middleware/verifyRoles');
 
 router.get('/', verifyRoles(ROLES_LIST.Manager), customers.getCusomter);
 router.post('/signup', verifyRoles(ROLES_LIST.Manager, ROLES_LIST.Employee), customers.signUp);
-router.post('/login', verifyRoles(ROLES_LIST.Manager, ROLES_LIST.Customer), customers.login);
 router.put('/updatePassword', verifyRoles(ROLES_LIST.Manager, ROLES_LIST.Customer), customers.updatePassword);
 router.get('/expense/:id', verifyRoles(ROLES_LIST.Manager, ROLES_LIST.Customer), customers.getExpense);
 router.get('/rank', verifyRoles(ROLES_LIST.Manager), customers.rankCustomer);
 router.get('/potential', verifyRoles(ROLES_LIST.Manager), customers.potentialCustomer);
-router.get('/order/:id', verifyRoles(ROLES_LIST.Manager), customers.orderCustomer);
+router.get('/order/:id', verifyRoles(ROLES_LIST.Manager, ROLES_LIST.Customer), customers.orderCustomer);
 module.exports = router;
