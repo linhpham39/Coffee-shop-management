950df95f6a0bf3a70d4d5179b78f648e1f2dfcc5
linhpham39 <linhshark02@gmail.com>
2023-02-22 16:49:06 +0700

    update data for ingredients

--- data/ingredients.sql
+++ data/ingredients.sql
@@ -1,9 +1,10 @@
+
 INSERT INTO ingredients
 VALUES
-(1, 'Milk',1.42, 3.5),
-(2,'Espresso Roast',  50.2, 2.1),
-(3, 'Sugar', 2.1, 2.3),
-(4, 'Butter',15.4,  0.6),
-(5, 'Pistachios',23.4,  1.4),
-(6, 'Chocolate',22.3,  1),
-(7, 'Flour',1.6,  2.6);
+(1, 1.42, 'Milk', 3.5),
+(2, 50.2, 'Espresso Roast', 2.1),
+(3, 2.1, 'Sugar', 2.3),
+(4, 15.4, 'Butter', 0.6),
+(5, 23.4, 'Pistachios', 1.4),
+(6, 22.3, 'Chocolate', 1),
+(7, 1.6, 'Flour', 2.6);
--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    database: "coffeeshop",
-    password: "425222",
+    database: "Coffee Shop Management",
+    password: "123123",
     port: 5432
 });
 
