b916d6d917437833897285b3febe3f7f863d9c6a
linhpham39 <linhshark02@gmail.com>
2023-02-22 13:32:53 +0700

    recommit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -38,7 +38,7 @@ const handleLogin = async (req, res) => {
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '30m' }
+            { expiresIn: '10m' }
         );
         const refreshToken = jwt.sign(
             { "username": foundUser.username },
--- src/queries/customerQueries.js
+++ src/queries/customerQueries.js
@@ -3,13 +3,9 @@ const getCustomerByEmail = 'SELECT email, password FROM customers WHERE email = 
 const addCustomer = 'INSERT INTO customers VALUES($1, $2, $3, $4, $5, $6, $7)';
 const updatePassword = 'UPDATE customers SET password = $1 WHERE email = $2';
 const getExpense = 'select * from count_expense_customer($1)';
-<<<<<<< HEAD
-const mostConsumedCus = 'select * from high_expense_customer()';
-=======
 const rankCustomer = 'select * from high_expense_customer()';
 const potentialCustomer = 'select * from potential_customer($1,$2)';
 const orderCustomer = 'select * from customer_orders($1)';
->>>>>>> 199fc633b1ee3394fa7c43227429b9a86ddf19dd
 
 module.exports = {
     getCustomers,
@@ -17,11 +13,7 @@ module.exports = {
     addCustomer,
     updatePassword,
     getExpense,
-<<<<<<< HEAD
-    mostConsumedCus
-=======
     rankCustomer,
     potentialCustomer,
     orderCustomer
->>>>>>> 199fc633b1ee3394fa7c43227429b9a86ddf19dd
 }
