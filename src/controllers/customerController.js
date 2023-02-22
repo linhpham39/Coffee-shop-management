bd91cdfdd3cdf382f2f3d91cec2dc547011bca72
Minh Huyen <huyen.nm206652@sis.hust.edu.vn>
2023-02-22 19:43:07 +0700

    update password

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,4 +1,4 @@
-const pool  = require('../../db');
+const pool = require('../../db');
 const queries = require('../queries/customerQueries');
 const usersDB = {
     users: require('../model/users.json'),
@@ -8,9 +8,9 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 //liet ke tat ca cac khach hang
-const getCusomter = (req, res)=>{
-    pool.query(queries.getCustomers, (error, results) =>{
-        if(error) throw error;
+const getCusomter = (req, res) => {
+    pool.query(queries.getCustomers, (error, results) => {
+        if (error) throw error;
         res.status(200).json(results.rows);
     });
 };
@@ -18,12 +18,12 @@ const getCusomter = (req, res)=>{
 
 //dang ky tai khoan cho customer, neu email bi trung roi thi thong bao loi
 const signUp = (req, res) => {
-    const {customer_id, name, dob, email, password, rank, expense} = req.body;
-    pool.query(queries.getCustomerByEmail, [email], (error, results)=> {
-        if(error)   throw error;
-        if(results.rows.length <= 0){
-            pool.query(queries.addCustomer, [customer_id, name, dob, email, password, rank, expense], async (error, results)=>{
-                if(error)   throw error;
+    const { customer_id, name, dob, email, password, rank, expense } = req.body;
+    pool.query(queries.getCustomerByEmail, [email], (error, results) => {
+        if (error) throw error;
+        if (results.rows.length <= 0) {
+            pool.query(queries.addCustomer, [customer_id, name, dob, email, password, rank, expense], async (error, results) => {
+                if (error) throw error;
                 const newUser = {
                     "username": email,
                     "roles": { "Customer": 3000 },
@@ -36,38 +36,46 @@ const signUp = (req, res) => {
                 );
                 return res.status(200).json('Successfully registered');
             })
-        }else{
+        } else {
             return res.status(400).json('Email already exist');
         }
-    }) 
+    })
 }
 
 //dang nhap
-const login = (req, res)=>{
-    const {email, password} = req.body;
-    pool.query(queries.getCustomerByEmail, [email], (error, results) =>{
-        if(error) throw error;
-        if(!results.rows.length || results.rows[0].password != password)
+const login = (req, res) => {
+    const { email, password } = req.body;
+    pool.query(queries.getCustomerByEmail, [email], (error, results) => {
+        if (error) throw error;
+        if (!results.rows.length || results.rows[0].password != password)
             res.status(401).json('Incorrect Username or Password');
-        else if(results.rows[0].password == password)
+        else if (results.rows[0].password == password)
             res.status(200).json('Login successfully');
 
     })
 }
 
 //đổi mật khẩu
-const updatePassword = (req, res) =>{
-    const {email, currentPassword, newPassword} = req.body;
-    pool.query(queries.getCustomerByEmail, [email], (error, results) =>{
-        if(error) throw error;
-        if(!results.rows.length || results.rows[0].password != currentPassword)
+const updatePassword = (req, res) => {
+    const { email, currentPassword, newPassword } = req.body;
+    pool.query(queries.getCustomerByEmail, [email], (error, results) => {
+        if (error) throw error;
+        if (!results.rows.length || results.rows[0].password != currentPassword)
             res.status(401).json('Incorrect Username or old Password');
-        else if(results.rows[0].password == currentPassword)
+        else if (results.rows[0].password == currentPassword)
             //nếu nhập mật khẩu hiện tại đúng => đổi sang mật khẩu mới
-            pool.query(queries.updatePassword, [newPassword, email], (error, results)=>{
-                if(error) throw error;
-                console.log(newPassword);
-                res.status(200).json('Update password successfully!');  
+            pool.query(queries.updatePassword, [newPassword, email], async (error, results) => {
+                if (error) throw error;
+                // console.log(newPassword);
+                else {
+                    const foundUser = usersDB.users.find(person => person.username === email);
+                    foundUser.password = newPassword;
+                    await fsPromises.writeFile(
+                        path.join(__dirname, '..', 'model', 'users.json'),
+                        JSON.stringify(usersDB.users)
+                    );
+                    res.status(200).json('Update password successfully!')
+                };
 
             })
 
@@ -77,32 +85,32 @@ const updatePassword = (req, res) =>{
 //cập nhật lại rank done 
 
 //customer xem minh da tieu bnhieu
-const getExpense = (req, res) =>{
+const getExpense = (req, res) => {
     var id = req.params.id;
-    pool.query(queries.getExpense, [id], (error, results) =>{
+    pool.query(queries.getExpense, [id], (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows);
     })
 }
 
-const rankCustomer = (req, res) =>{
-    pool.query(queries.rankCustomer, (error, results) =>{
-        if(error) throw error;
+const rankCustomer = (req, res) => {
+    pool.query(queries.rankCustomer, (error, results) => {
+        if (error) throw error;
         res.status(200).json(results.rows);
     })
 }
 
-const potentialCustomer = (req, res) =>{
-    const {date1, date2} = req.body;
-    pool.query(queries.potentialCustomer, [date1, date2], (error, results)=>{
-        if(error) throw error;
+const potentialCustomer = (req, res) => {
+    const { date1, date2 } = req.body;
+    pool.query(queries.potentialCustomer, [date1, date2], (error, results) => {
+        if (error) throw error;
         res.status(200).json(results.rows);
     })
 }
 
-const orderCustomer = (req, res) =>{
+const orderCustomer = (req, res) => {
     var id = req.params.id;
-    pool.query(queries.orderCustomer, [id], (error, results) =>{
+    pool.query(queries.orderCustomer, [id], (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows);
     })
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,6 +6,7 @@ const getAllProducts = (req, res) =>{
     pool.query(queries.getProducts, (error, results)=>{
         if(error) throw error;
         res.status(200).json(results.rows);
+        // console.log(pool.user);
     }) 
 }
 
@@ -18,9 +19,9 @@ const getProductById = (req, res) => {
 }
 
 const addProduct = (req, res) =>{
-    const {product_id, name, category, selling_price, state, revenue} = req.body;
+    const {product_id, name, category, selling_price, status, profit} = req.body;
     console.log(1);
-    pool.query(queries.addProduct, [product_id, name, category, selling_price, state, revenue], (error, results)=>{
+    pool.query(queries.addProduct, [product_id, name, category, selling_price, status, profit], (error, results)=>{
         if(error) throw error; 
         res.status(200).send('Successfully add product');
     })
@@ -41,13 +42,13 @@ const removeProduct= (req, res) => {
 };
 
 const updateProduct = (req, res) =>{
-    const {product_id, name, category, selling_price, state, profit} = req.body;
+    const {product_id, name, category, selling_price, status, profit} = req.body;
     pool.query(queries.getProductById, [product_id], (error, results) =>{
         if(!results.rows.length)
             res.send("Product does not exist in the database");
         
         //update product
-        pool.query(queries.updateProduct, [product_id, name, category, selling_price, state, profit], (error, results) =>{
+        pool.query(queries.updateProduct, [product_id, name, category, selling_price, status, profit], (error, results) =>{
             if(error) throw error;
             res.status(200).send('Update successfully!');
         });
